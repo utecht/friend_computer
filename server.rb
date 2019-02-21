ea22768096c153c5d4850d9106be36b57a774f68
Joseph Utecht <joseph.utecht@gmail.com>
2019-02-21 11:29:11 -0600

    initial version

---
+++ server.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+
+require "sinatra"
+require "json"
+require "git"
+require "fileutils"
+require "nokogiri"
+require "mail"
+
+if ENV.has_key? "FC_MAIL"
+  Mail.defaults do
+    delivery_method :smtp, address: ENV["FC_MAIL"], port: 25
+  end
+else
+  puts "FC_MAIL ENV not set, mail sending disabled"
+end
+
+class Release
+  attr_reader :ref, :pusher, :repo_name, :versionIRI, :versionInfo
+
+  def initialize(name, payload)
+    @base_path = "/tmp/friend_computer"
+    @name = name
+    @ref = payload["ref"]
+    @url = payload["repository"]["html_url"]
+    @repo_name = payload["repository"]["name"]
+    @pusher = payload["sender"]["login"]
+  end
+
+  def clone_repo
+    cleanup
+    @repo = Git.clone(@url, @repo_name, path: @base_path)
+    @repo.fetch
+    @repo.checkout(@ref)
+  end
+
+  def check_version
+    owl = File.open("#{@base_path}/#{@repo_name}/#{@name}.owl") { |f| Nokogiri::XML(f) }
+    iri = owl.xpath('//owl:versionIRI/@rdf:resource').first
+    @versionIRI = iri.value unless iri == nil
+    info = owl.xpath('//owl:versionInfo').first
+    @versionInfo = info.content unless info == nil
+  end
+
+  def cleanup
+    FileUtils.remove_dir("#{@base_path}/#{@repo_name}", :force => true)
+  end
+end
+
+post "/payload/:name" do |name|
+  if request.env["HTTP_X_GITHUB_EVENT"] == "create"
+    payload = JSON.parse(request.body.read)
+    return 200 unless payload["ref_type"] == "tag"
+    release = Release.new(name, payload) if payload["ref_type"] == "tag"
+    release.clone_repo
+    release.check_version
+    release.cleanup
+    message = %[New Release Detected for #{release.repo_name} created by #{release.pusher}
+
+Release Name: #{release.ref}
+VersionIRI: #{release.versionIRI}
+VersionInfo: #{release.versionInfo}]
+    puts message
+    if ENV.has_key? "FC_MAIL"
+      Mail.deliver do
+        from 'friend_computer@cafe-trauma.com'
+        to 'jrutecht@uams.edu'
+        subject "New Release #{release.repo_name}"
+        body message
+      end
+      puts "Mail dispatched"
+    end
+  else
+    puts "Non-create request sent"
+  end
+end
